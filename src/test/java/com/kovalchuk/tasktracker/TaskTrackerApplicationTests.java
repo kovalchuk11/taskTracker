1f0052e9983a130e58d91c92d56f3f3887591811
Ivan Kovalchuk <kovalchuk427@gmail.com>
2020-06-28 19:41:43 +0300

    add pom

---
+++ src/test/java/com/kovalchuk/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovalchuk.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
