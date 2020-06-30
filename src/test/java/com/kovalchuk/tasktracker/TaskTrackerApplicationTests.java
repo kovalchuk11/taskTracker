93c60590338c4ae18b4c8cfa7a1dab5c88f6dc46
Ivan Kovalchuk <kovalchuk427@gmail.com>
2020-07-01 02:09:25 +0300

    add Access test

--- src/test/java/com/kovalchuk/tasktracker/TaskTrackerApplicationTests.java
+++ src/test/java/com/kovalchuk/tasktracker/TaskTrackerApplicationTests.java
@@ -1,20 +1,14 @@
 package com.kovalchuk.tasktracker;
 
-import com.kovalchuk.tasktracker.jwt.JwtConfig;
-import com.kovalchuk.tasktracker.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.crypto.SecretKey;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
